aa501a0a5434bb1abceb8306613af37903c0dd09
gbr5023 <gbr5023@E208-WD19.up.ist.local>
2017-09-14 17:26:30 -0400

    sept 14

---
+++ FoodMoodV1/src/userprofilemodel/User.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package userprofilemodel;
+
+/**
+ *
+ * @author gbr5023
+ */
+public class User 
+{
+    public String username;
+    public String password;
+    
+    /**
+     * This is the default constructor for the User class
+     */
+    public User()
+    {
+        
+    }
+}
