18b62e5225ab8fb61103ad162f73e9f601cc9a76
gbr5023 <gbr5023@E208-WD19.up.ist.local>
2017-09-14 17:42:13 -0400

    sept 14

---
+++ src/userprofilemodel/User.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package userprofilemodel;
+
+/**
+ *
+ * @author gbr5023
+ */
+public class User 
+{
+    public String username;
+    public String password;
+    
+    /**
+     * This is the default constructor for the User class
+     */
+    public User()
+    {
+        
+    }
+    
+    /**
+     * This is the method that authenticates the User
+     * @param username This is the User's username to authenticate
+     * @param password This is the User's password to authenticate
+     */
+    public boolean authenticate(String username, String password)
+    {
+        return true;
+    }
+}
