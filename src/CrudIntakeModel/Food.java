8875eb883f839e3723e9e07a5c1e05a165e2428d
GISELLE\Gisward <redila.giselle@gmail.com>
2017-09-17 05:02:14 -0400

    made controller packages for all 6 use cases,
    edited model classes: created a user interface, methods that all users share and created user model classes that are specific to a type of user,
    
    added new model class to CrudIntakeModel (in addition to food, intake also includes drinks- which I included)
    
    also added controllers for Login and Main Menu (NavigationCntl)

---
+++ src/CrudIntakeModel/Food.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CrudIntakeModel;
+
+/**
+ *
+ * @author hmk5180
+ */
+public class Food {
+    public String foodname;
+    public double foodWeight;
+    public int timeSinceConsumed;
+    
+    /**
+     * This is the default constructor for the Food class
+     */
+    public Food() {
+        
+    }
+    
+    /**
+     * This is the method that captures food intake
+     * @param foodname This is the Food's name to capture
+     * @param foodWeight This is the Food's weight (in oz) to capture
+     * @param timeSinceConsumed This is the time since last consumed (in min) of the Food
+     */
+    public void captureFood(String foodname, double foodWeight, int timeSinceConsumed) {
+        
+    }
+}
